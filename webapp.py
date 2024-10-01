a40932683c6270b83f65778f1cf40c60c0fdbe46
ZaydenF <zaydenfleming@gmail.com>
2024-10-01 13:25:14 -0700

    add files

--- webapp.py
+++ webapp.py
@@ -1,19 +1,14 @@
-from flask import Flask, url_for, render_template, response
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/response")
+@app.route("/")
 def render_main():
-    return render_template('home.html')
-
+    return render_template('index.html')
+    
 @app.route("/response")
-def render_page1():
-    # color request.arg 
-    return render_template('page1.html')
-
-@app.route("/p2")
-def render_page2():
-    return render_template('page2.html')
+def render_response():
+    swimtime 
     
 if __name__=="__main__":
     app.run(debug=False)
